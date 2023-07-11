8e002851255ddf93155285d0f5674201a5dc121f
DianaFarhat <39864218+DianaFarhat@users.noreply.github.com>
2023-07-11 13:02:45 +0300

    Add files via upload

---
+++ example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h> // for exit(-1)
+
+#define NUM_THREADS 5
+
+void *printHello(void * index) {
+    printf("\n%d: Hello CMPS 272!\n", *((int *) index));
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+    int t, err;
+    int ids[NUM_THREADS];
+    for(t = 0; t < NUM_THREADS; t++) {
+        ids[t] = t;
+        printf("Creating thread %d\n", t);
+        err = pthread_create(&threads[t], NULL, printHello, &ids[t]); 
+        if(err != 0) exit(-1);
+    }
+
+    pthread_exit(NULL); // Question? replace with exit(0);
+}
